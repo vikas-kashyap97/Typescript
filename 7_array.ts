5e41bad3d5102e2fc881d3b3be9c28bae707dde7
Vikas Kashyap <159269579+vikas-kashyap97@users.noreply.github.com>
2025-08-28 13:38:26 +0530

    commit

---
+++ 7_array.ts
@@ -0,0 +1,9 @@
+// const numbers: number[] = [1, 2, 3, 4, 5];
+// or
+// const number1: number[] = new Array (6, 7, 8, 9, 10);
+// or
+const names: string[] = new Array ("Vikas", "John", "Doe");
+
+console.log(names[2]);
+console.log(names.length);
+
